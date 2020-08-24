fe26370080b6074b3b6434323da39f4c1ed80879
Divyagb29 <divya2229mgb@gmail.com>
2020-08-24 15:38:58 +0530

    used method to get win/lose randomly

--- GamblingSimulator2.java
+++ GamblingSimulator2.java
@@ -1,7 +1,13 @@
+import java.util.Random;
 public class GamblingSimulator2
 {
 	static int stake = 100;
 	static int bet = 1;
+	public static int generateRandomNo()
+	{
+		Random random=new Random();
+		return random.nextInt(2);
+	}
 	public static void main(String ag[])
 	{
 		  System.out.println("Welcome to GamblingSimulator Game");

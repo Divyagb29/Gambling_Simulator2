54166a7501ee54e667a607534c1db6874ded8881
Divyagb29 <divya2229mgb@gmail.com>
2020-08-24 16:05:18 +0530

    Gambler if won or lost

--- GamblingSimulator2.java
+++ GamblingSimulator2.java
@@ -3,13 +3,41 @@ public class GamblingSimulator2
 {
 	static int stake = 100;
 	static int bet = 1;
+	static int stopDayLoss=50;
+	static int stopDayWin=150;
 	public static int generateRandomNo()
 	{
 		Random random=new Random();
 		return random.nextInt(2);
 	}
+	public static void resignOfTheDay()
+	{
+
+		while(stake > 50 && stake < 150)
+		{
+			int random=generateRandomNo();
+
+			if(random == 0)
+			{
+				stake--;
+			}
+			else
+			{
+				stake++;
+			}
+		}
+		if(stake == stopOfDayLoss)
+		{
+			System.out.println("He LOST for a day----> $"+stake);
+		}
+		else
+		{
+			System.out.println("He WON for the day----> $"+(stake-100));
+		}
+	}
 	public static void main(String ag[])
 	{
 		  System.out.println("Welcome to GamblingSimulator Game");
+		  resignOfTheDay();
 	}
 }

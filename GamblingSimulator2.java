698aecb07aa4f9579861d09f43c89a5cf9a093a5
Divyagb29 <divya2229mgb@gmail.com>
2020-08-24 16:48:37 +0530

    total amount win/lost for 20 days

--- GamblingSimulator2.java
+++ GamblingSimulator2.java
@@ -5,39 +5,69 @@ public class GamblingSimulator2
 	static int bet = 1;
 	static int stopDayLoss=50;
 	static int stopDayWin=150;
+	static int twentyDayloss = 0;
+	static int twentyDayWin = 0;
+	static int day = 0;
 	public static int generateRandomNo()
 	{
 		Random random=new Random();
 		return random.nextInt(2);
 	}
-	public static void resignOfTheDay()
+	public static void getTwentyDaysOfPlay()
 	{
-
-		while(stake > 50 && stake < 150)
+		while(day < 20)
 		{
-			int random=generateRandomNo();
+			stake = 100;
+			//per day report start
+			while(stake > 50 && stake < 150)
+			{
+				int random = generateRandomNo();
 
-			if(random == 0)
-			{
-				stake--;
+				if(random == 0)
+					stake--;
+
+				else
+					stake++;
 			}
-			else
+			//per day report end
+			if(stake == stopDayLoss)
 			{
-				stake++;
+				System.out.println("He LOST for a day"+(day+1)+" is $"+stake);
+				twentyDayloss = twentyDayloss+50;
+				day++;
+
+			}
+			else 
+			{
+				System.out.println("He win for day "+(day+1)+" is $"+stake);
+				twentyDayWin = twentyDayWin+50;
+				day++;
 			}
 		}
-		if(stake == stopOfDayLoss)
+
+		System.out.println();
+		System.out.println("20 days loss is $"+twentyDayloss);
+		System.out.println("20 days wins is $"+twentyDayWin);
+		
+		if(twentyDayloss > twentyDayWin)
 		{
-			System.out.println("He LOST for a day----> $"+stake);
+			//to calculate 20 days loss
+			twentyDayloss = twentyDayloss-twentyDayWin;
+			System.out.println();
+			System.out.println("overall loss in 20 days  is $"+twentyDayloss);
 		}
 		else
-		{
-			System.out.println("He WON for the day----> $"+(stake-100));
+		{ 
+	        //to calculate 20 days win
+			twentyDayWin = twentyDayWin-twentyDayloss;
+			System.out.println();
+			System.out.println("overall profit in a 20 days  is $"+twentyDayWin);
 		}
+		
 	}
 	public static void main(String ag[])
 	{
 		  System.out.println("Welcome to GamblingSimulator Game");
-		  resignOfTheDay();
+		  getTwentyDaysOfPlay();
 	}
 }
